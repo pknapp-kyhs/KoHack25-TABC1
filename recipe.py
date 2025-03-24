4c52295585350d8f3a2e373f3379e7c2d495b93a
GabeTABC <rosenbluthg28@tabc.org>
2025-03-24 12:50:06 -0400

    Delete recipe.py

--- recipe.py
+++
@@ -1,8 +0,0 @@
-recipe_name = input('Enter your recipe name: ')
-recipe_region = input("Enter the recipes' region: ")
-recipe_culture = input('What culture is this recipe from: ')
-data_list = [
-    recipe_name,
-    recipe_region,
-    recipe_culture
-]
